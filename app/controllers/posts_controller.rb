2e28ec4c7b503a7065921876db57100ca605b2f2
Shankar Poncelet <shankx@mac.com>
2016-04-07 20:16:41 -0500

    assignment-31-crud: Completed

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all
+    @posts = clean_array Post.all
   end
 
   def show
@@ -11,4 +11,14 @@ class PostsController < ApplicationController
 
   def edit
   end
+
+  def clean_array array
+    array.each do |el|
+      if (array.index(el) % 5 == 0)
+        el.title = "Sanitized Post Title"
+        el.save
+      end
+    end
+  end
+
 end
