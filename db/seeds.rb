ec690ae931e91f2ea6cc7304ba21fb0ec7a81565
Shankar Poncelet <shankx@mac.com>
2016-04-10 23:07:14 -0500

    assignment-32-crud: Completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,6 +21,17 @@ require 'random_data'
    )
  end
 
+
+
+ 50.times do
+   Advertisement.create!(
+   title:  RandomData.random_sentence,
+   body:   RandomData.random_paragraph,
+   price: rand(1000)
+   )
+ end
+
  puts "Seed finished"
  puts "#{Post.count} posts created"
  puts "#{Comment.count} comments created"
+ puts "#{Advertisement.count} advertisements created."
