6b387f203887aeaac5fd51833d9a56340734b031
Shankar Poncelet <shankx@mac.com>
2016-05-02 07:12:52 -0500

    Checkpoint 36: Complete

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160426141835) do
+ActiveRecord::Schema.define(version: 20160502115834) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "body"
@@ -32,12 +32,31 @@ ActiveRecord::Schema.define(version: 20160426141835) do
 
   add_index "posts", ["topic_id"], name: "index_posts_on_topic_id"
 
+  create_table "sponsored_posts", force: :cascade do |t|
+    t.string   "title"
+    t.text     "body"
+    t.integer  "price"
+    t.integer  "topic_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "sponsored_posts", ["topic_id"], name: "index_sponsored_posts_on_topic_id"
+
   create_table "topics", force: :cascade do |t|
     t.string   "name"
     t.boolean  "public",      default: true
     t.text     "description"
     t.datetime "created_at",                 null: false
     t.datetime "updated_at",                 null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
