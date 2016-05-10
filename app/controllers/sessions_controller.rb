acf71bf6fe173e12b2501becf1a8c9383242500a
Shankar Poncelet <shankx@mac.com>
2016-05-09 22:40:24 -0500

    Assignment 38:Completed

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,6 +7,7 @@ class SessionsController < ApplicationController
 
     if user && user.authenticate(params[:session][:password])
       create_session(user)
+      avatar_url(user)
       flash[:notice] = "Welcome, #{user.name}!"
       redirect_to root_path
     else
