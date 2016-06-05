b6c4b530a8de865147e22395436cb55848651528
Shankar Poncelet <shankx@mac.com>
2016-06-05 09:40:01 -0500

    Checkpoint 43: Completed

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160525112138) do
+ActiveRecord::Schema.define(version: 20160605142927) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "body"
@@ -48,21 +48,11 @@ ActiveRecord::Schema.define(version: 20160525112138) do
     t.datetime "updated_at", null: false
     t.integer  "topic_id"
     t.integer  "user_id"
+    t.float    "rank"
   end
 
   add_index "posts", ["topic_id"], name: "index_posts_on_topic_id"
   add_index "posts", ["user_id"], name: "index_posts_on_user_id"
-
-  create_table "sponsored_posts", force: :cascade do |t|
-    t.string   "title"
-    t.text     "body"
-    t.integer  "price"
-    t.integer  "topic_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  add_index "sponsored_posts", ["topic_id"], name: "index_sponsored_posts_on_topic_id"
 
   create_table "topics", force: :cascade do |t|
     t.string   "name"
@@ -80,5 +70,16 @@ ActiveRecord::Schema.define(version: 20160525112138) do
     t.datetime "updated_at",      null: false
     t.integer  "role"
   end
+
+  create_table "votes", force: :cascade do |t|
+    t.integer  "value"
+    t.integer  "user_id"
+    t.integer  "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "votes", ["post_id"], name: "index_votes_on_post_id"
+  add_index "votes", ["user_id"], name: "index_votes_on_user_id"
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -22,44 +22,48 @@ topics = Topic.all
 
 # Create Posts
 50.times do
-  # #1
-  Post.create!(
+  post = Post.create!(
   user:   users.sample,
   topic:  topics.sample,
   title:  RandomData.random_sentence,
   body:   RandomData.random_paragraph
   )
-end
-posts = Post.all
 
-# Create Comments
-# #3
-100.times do
-  Comment.create!(
-  # #4
-  post: posts.sample,
-  body: RandomData.random_paragraph,
-  user: users.sample,
-  )
-end
+  post.update_attribute(:created_at, rand(10.minutes .. 1.year).ago)
+  rand(1..5).times { post.votes.create!(value: [-1, 1].sample, user: users.sample) }
+  end
+  posts = Post.all
 
-# Create an admin user
-admin = User.create!(
+
+  # Create Comments
+  # #3
+  100.times do
+    Comment.create!(
+    # #4
+    post: posts.sample,
+    body: RandomData.random_paragraph,
+    user: users.sample,
+    )
+  end
+
+  # Create an admin user
+  admin = User.create!(
   name:     'Admin User',
   email:    'admin@example.com',
   password: 'helloworld',
   role:     'admin'
-)
+  )
 
-# Create a member
-member = User.create!(
+  # Create a member
+  member = User.create!(
   name:     'Member User',
   email:    'member@example.com',
   password: 'helloworld'
-)
+  )
 
-puts "Seed finished"
-puts "#{User.count} users created"
-puts "#{Topic.count} topics created"
-puts "#{Post.count} posts created"
-puts "#{Comment.count} comments created"
+  puts "Seed finished"
+  puts "#{User.count} users created"
+  puts "#{Topic.count} topics created"
+  puts "#{Post.count} posts created"
+  puts "#{Comment.count} comments created"
+  puts "#{Vote.count} votes created"
