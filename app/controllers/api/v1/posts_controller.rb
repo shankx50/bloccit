a7a41d5aebd7a05167921bea6971ad0e7301a8cb
Shankar Poncelet <shankx@mac.com>
2016-06-15 10:44:47 -0500

    Assignment 49: Completed

---
+++ app/controllers/api/v1/posts_controller.rb
@@ -0,0 +1,45 @@
+class Api::V1::PostsController < Api::V1::BaseController
+  before_action :authenticate_user, except: [:index, :show]
+  before_action :authorize_user, except: [:index, :show]
+
+  def update
+    post = Post.find(params[:id])
+    post.assign_attributes(post_params)
+
+    if post.save
+      render json: post, status: 200
+    else
+      render json: { error: 'Post update failed', status: 400 }, status: 400
+    end
+
+  end
+
+  def create
+    topic = Topic.find(params[:topic_id])
+    post = topic.posts.build(post_params)
+    post.user = @current_user
+
+    if post.save
+      render json: post, status: 200
+    else
+      render json: { error: 'Post is invalid', status: 400 }, status: 400
+    end
+  end
+
+  def destroy
+    post = Post.find(params[:id])
+
+    if post.destroy
+      render json: {message: "Post destroyed", status: 200}, status: 200
+    else
+      render json: {error: "Post destroy failed", status: 400}, status: 400
+    end
+
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
+end
